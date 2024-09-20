6dc689915e2a8bc3bda91ad9c64c5ac52c2a0cc0
Shubham Rawat <sr856161@gmail.com>
2024-09-20 22:41:40 +0530

    change vite build folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
 export default defineConfig({
+  build:{
+    outDir:'./build',
+    emptyOutDir: true
+  },
   plugins: [react()],
   resolve: {
     alias: {
