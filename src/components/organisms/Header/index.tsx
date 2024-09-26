2c33e844f00aa5b4db83935d2e2d043fcc21d3c9
Shubham Rawat <sr856161@gmail.com>
2024-09-26 12:01:10 +0530

    add dropdown in profile click

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,15 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { IoIosLogOut } from "react-icons/io";
 import { FiMenu } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header({ toggleMenu }: { toggleMenu: () => void }) {
   const user = {
     name: "Full Name",
     avatar: "https://github.com/shadcn.png",
-    initials: "FN"
+    initials: "FN",
   };
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleAvatarClick = () => {
     navigate("/dashboard/profile");
@@ -26,17 +32,27 @@ export default function Header({ toggleMenu }: { toggleMenu: () => void }) {
         <p className="text-3xl">PVX</p>
       </div>
       <div className="flex justify-center items-center">
-        {/* Added onClick handler and cursor-pointer class to Avatar */}
-        <Avatar onClick={handleAvatarClick} className="cursor-pointer">
-          {/* Updated src and alt attributes to use user object properties */}
-          <AvatarImage src={user.avatar} alt={user.name} />
-          <AvatarFallback>{user.initials}</AvatarFallback>
-        </Avatar>
-        {/* Updated to use user.name */}
-        <p className="ml-4">{user.name}</p>
-        <div className="flex justify-center items-center gap-2 ml-8">
-          <IoIosLogOut size={"30px"} />
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex justify-center items-center">
+            <Avatar className="cursor-pointer">
+              <AvatarImage src={user.avatar} alt={user.name} />
+              <AvatarFallback>{user.initials}</AvatarFallback>
+            </Avatar>
+            <p className="ml-4 hidden sm:block">{user.name}</p>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-2">
+            {/* <DropdownMenuLabel className="block sm:hidden">
+              Hi {user.name} !
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator className="block sm:hidden" /> */}
+            <DropdownMenuItem className="px-8" onClick={handleAvatarClick}>
+              View Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="px-8">Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="px-8">Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
